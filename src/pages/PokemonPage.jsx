7ba8e2cd68fb681c755d5cc9256869d48069a06d
diegohenao819 <diegohenao819@gmail.com>
2024-01-04 16:37:25 -0500

    Add PokemonPage CSS and update app.css

--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -2,6 +2,7 @@ import { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { PokemonContext } from "../context/PokemonContext";
 import Loader from "../components/Loader";
+import "../assets/css/PokemonPage.css";
 
 const PokemonPage = () => {
   const { getPokemonById } = useContext(PokemonContext);
@@ -28,91 +29,145 @@ const PokemonPage = () => {
         <Loader />
       ) : (
         <>
-          <div>
-            <span>#{pokemon.id}</span>
-
-            <div>
-              <img
-                src={pokemon.sprites.other.dream_world.front_default}
-                alt={`Pokemon ${pokemon?.name}`}
-              />
+          <div className="PokemonPageContainer">
+            <div className="spanId">
+              <span className="PokemonId">#{pokemon.id}</span>
             </div>
 
-            <div className="container-info-pokemon">
-              <h1>{(pokemon.name).charAt(0).toUpperCase() + (pokemon.name).slice(1)}</h1>
-              <div>
-                {pokemon.types.map((type) => (
-                  <span key={pokemon.id}>{type.type.name}</span>
-                ))}
-              </div>
-
-              <div className="info-pokemon">
-                <div className="group-info">
-                  <p>Altura</p>
-                  <span>{pokemon.height}</span>
+            {/* CONTAINER PRINCIPAL */}
+            <div className="main-info">
+              {/* Name & Type*/}
+              <div className="container-info-pokemon">
+                <h1>
+                  {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                </h1>
+                <div className="type">
+                  {pokemon.types.map((type) => (
+                    <span
+                      key={pokemon.id}
+                      className={`button-type ${type.type.name}`}
+                    >
+                      {type.type.name}
+                    </span>
+                  ))}
                 </div>
-
-                <div className="group-info">
-                  <p>Peso</p>
-                  <span>{pokemon.weight} kg</span>
+              </div>
+              {/* Peso & Altura */}
+              <div className="group-info">
+                <div className="altura">
+                  <p className="height">Height</p>
+                  <p className="dato">{pokemon.height} feet</p>
+                </div>
+                <div className="peso">
+                  <p className="weight">Weight</p>
+                  <p className="dato">{pokemon.weight} kg</p>
                 </div>
               </div>
-
-
-
-              <div className="container-stats">
-                  <h1>Stats</h1>
-                  <div className="stats">
-                   <div className="stat-group">
-                     <span>hp</span>
-                     <div className="progress-bar"> . </div>
-                     <span className="counter-stat"> {pokemon.stats[0].base_stat} </span>
-                   </div>
-
-                   <div className="stat-group">
-                     <span>Attack</span>
-                     <div className="progress-bar"> . </div>
-                     <span className="counter-stat"> {pokemon.stats[1].base_stat} </span>
-                   </div>
-
-                   <div className="stat-group">
-                     <span>Defense</span>
-                     <div className="progress-bar"> . </div>
-                     <span className="counter-stat"> {pokemon.stats[2].base_stat} </span>
-                   </div>
-
-                   <div className="stat-group">
-                     <span>Special Attack</span>
-                     <div className="progress-bar"> . </div>
-                     <span className="counter-stat"> {pokemon.stats[3].base_stat} </span>
-                   </div>
-
-                   <div className="stat-group">
-                     <span>Special Defense</span>
-                     <div className="progress-bar"> . </div>
-                     <span className="counter-stat"> {pokemon.stats[4].base_stat} </span>
-                   </div>
-
-                   
-                   <div className="stat-group">
-                     <span>Speed</span>
-                     <div className="progress-bar"> . </div>
-                     <span className="counter-stat"> {pokemon.stats[5].base_stat} </span>
-                   </div>
-                 
-                 
-                 
-                 
-                 
-                 
-                  </div>
-                  
+              {/* Image */}
+              <div className="picture">
+                <img
+                  src={pokemon.sprites.other.dream_world.front_default}
+                  alt={`Pokemon ${pokemon?.name}`}
+                />
               </div>
-
-
-
-
             </div>
+         
+
+          {/* CONTAINER OF STATS */}
+          <div className="container-stats">
+            <h1>STATS</h1>
+            <div className="stats">
+              <div className="stat-group">
+                <span>Hit Points</span>
+                <div className="progress-bar">
+                  <span
+                    className="counter-stat"
+                    style={{
+                      
+                      width: `${pokemon.stats[0].base_stat}%`
+                    }}
+                  >
+                    {pokemon.stats[0].base_stat}
+                  </span>
+                </div>
+              </div>
+
+              <div className="stat-group">
+                <span>Attack</span>
+                <div className="progress-bar">
+                  <span
+                    className="counter-stat"
+                    style={{
+                     
+                      width: `${pokemon.stats[1].base_stat}%`,
+                    }}
+                  >
+                    {pokemon.stats[1].base_stat}
+                  </span>
+                </div>
+              </div>
+
+              <div className="stat-group">
+                <span>Defense</span>
+                <div className="progress-bar">
+                  <span
+                    className="counter-stat"
+                    style={{
+                      
+                      width: `${pokemon.stats[2].base_stat}%`,
+                    }}
+                  >
+                    {pokemon.stats[2].base_stat}
+                  </span>
+                </div>
+              </div>
+
+              <div className="stat-group">
+                <span>Special Attack</span>
+                <div className="progress-bar">
+                  <span
+                    className="counter-stat"
+                    style={{
+                      
+                      width: `${pokemon.stats[3].base_stat}%`,
+                    }}
+                  >
+                    {pokemon.stats[3].base_stat}
+                  </span>
+                </div>
+              </div>
+
+              <div className="stat-group">
+                <span>Special Defense</span>
+                <div className="progress-bar">
+                  <span
+                    className="counter-stat"
+                    style={{
+                     
+                      width: `${pokemon.stats[4].base_stat}%`,
+                    }}
+                  >
+                    {pokemon.stats[4].base_stat}
+                  </span>
+                </div>
+              </div>
+
+              <div className="stat-group">
+                <span>Speed</span>
+                <div className="progress-bar">
+                  <span
+                    className="counter-stat"
+                    style={{
+                     
+                      width: `${pokemon.stats[5].base_stat}%`,
+                    }}
+                  >
+                    {pokemon.stats[5].base_stat}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
           </div>
         </>
       )}
